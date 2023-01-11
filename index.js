16e2e39ab18fffc613aa97f7fba5b789f2b53fdb
Noy <noyam2004@gmail.com>
2023-01-11 19:26:50 +0200

    part1

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const Bamadash = {
-    calc(operand1, operand2, operator) {
-        throw new Error("Method not implimented.");
-    },
-    compact(array){
-        throw new Error("Method not implimented.");
+    calc(operand1, operand2, operator) 
+    {
+        let answer;
+         if (operator = "*") answer = operand1 * operand2;
+         if (operator = "-") answer = operand1 - operand2;
+         if (operator = "+") answer = operand1 + operand2;
+         if (operator = "/") answer = operand1 / operand2;
+    } ,
+    compact(array)
+    {
+        array.forEach(element) 
+            while(element != null & element != false & element != 0 & element != NaN & element != "" & element != undefined)
+            {
+                console.log(element);
+            }
+
     },
     intersection(array1,array2, isStrict){
         throw new Error("Method not implimented.");
